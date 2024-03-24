625bac6cf8f808a17d37f773db33923bfccd3fac
xJunko <fireredz69420@gmail.com>
2024-03-24 21:02:01 +0800

    Script: New build tools

--- toolchains/scripts/build.sh
+++ toolchains/scripts/build.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 set -e # fuck off the moment shit doenst work
 
+mkdir build -p
+
 # build bootloader
-toolchains/i686-elf/bin/as src/boot/loader.s -o build/boot.o 
+toolchains/i686-elf/i686-elf/bin/as src/boot/loader.s -o build/boot.o 
 
 # build kernel
-toolchains/bin/i686-elf-gcc -c src/kernel/main.c -o build/kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
+toolchains/i686-elf/bin/i686-elf-gcc -c src/kernel/main.c -o build/kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
 
 # build os
-toolchains/bin/i686-elf-gcc -T src/boot/linker.ld -o build/rin_os.bin -ffreestanding -O2 -nostdlib build/boot.o build/kernel.o -lgcc
+toolchains/i686-elf/bin/i686-elf-gcc -T src/boot/linker.ld -o build/rin_os.bin -ffreestanding -O2 -nostdlib build/boot.o build/kernel.o -lgcc
 
 # check if shits legit
 if grub-file --is-x86-multiboot build/rin_os.bin; then
