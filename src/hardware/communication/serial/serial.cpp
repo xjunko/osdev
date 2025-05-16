6333747e9920ad94c3e727eb2d7dbebb7e959819
xjunko <fireredz69420@gmail.com>
2025-05-16 21:43:36 +0800

    Com/Serial: Implement COMLog for debugging purposes

---
+++ src/hardware/communication/serial/serial.cpp
@@ -0,0 +1,53 @@
+#include <commons/types.h>
+#include <hardware/communication/serial/serial.h>
+
+using namespace RinOS::Hardware::Communication::Serial;
+
+COMLog RinOS::Hardware::Communication::Serial::COM1(0x3F8);
+
+void RinOS::Hardware::Communication::Serial::InitializeGlobalCOM() {
+  RinOS::Hardware::Communication::Serial::COM1.Initialize();
+
+  COM1.Print("[COM] COM1 Initialized!");
+}
+
+COMLog::COMLog(u16 base_com)
+    : com_port_0(base_com),
+      com_port_1(base_com + 1),
+      com_port_2(base_com + 2),
+      com_port_3(base_com + 3),
+      com_port_4(base_com + 4),
+      com_port_5(base_com + 5) {}
+COMLog::~COMLog() {}
+
+void COMLog::Initialize() {
+  this->com_port_1.Write(0x0);
+  this->com_port_3.Write(0x80);
+  this->com_port_0.Write(0x03);
+  this->com_port_1.Write(0x0);
+  this->com_port_3.Write(0x03);
+  this->com_port_2.Write(0xC7);
+  this->com_port_4.Write(0x0B);
+}
+
+bool COMLog::IsReady() { return (this->com_port_5.Read() & 0x20) != 0; }
+
+void COMLog::Write(char c) {
+  while (!this->IsReady()) {
+  }
+  this->com_port_0.Write(c);
+}
+
+void COMLog::WriteString(char* str) {
+  while (*str) {
+    if (*str == '\n') {
+      this->Write('\r');
+    }
+    this->Write(*str++);
+  }
+}
+
+void COMLog::Print(char* str) {
+  this->WriteString(str);
+  this->Write('\n');
+}
