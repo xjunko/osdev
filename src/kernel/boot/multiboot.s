d48ffc2994a5eafe87a6e05f5f27d8556aad7fa5
xjunko <fireredz69420@gmail.com>
2025-05-25 14:43:33 +0800

    kernel: upgrade to multiboot2

--- src/kernel/boot/multiboot.s
+++ src/kernel/boot/multiboot.s
@@ -1,26 +1,41 @@
-// multiboot magic numbers
-.set ALIGN,     1 << 0
-.set MEMINFO,   1 << 1
-.set FLAGS,     ALIGN | MEMINFO
-.set MAGIC,     0x1BADB002
-.set CHECKSUM,  -(MAGIC + FLAGS)
+// multiboot2 stuffs
+.set MULTIBOOT2_HEADER_MAGIC,      0xE85250D6
+.set MULTIBOOT2_ARCHITECTURE_I386, 0
+.set MULTIBOOT2_HEADER_LENGTH,     multiboot2_header_end - multiboot2_header
+.set MULTIBOOT2_HEADER_CHECKSUM,   -(MULTIBOOT2_HEADER_MAGIC + MULTIBOOT2_ARCHITECTURE_I386 + MULTIBOOT2_HEADER_LENGTH)
 
-// multiboot
-.section .multiboot
-    .align 16, 0
-    .long MAGIC
-    .long FLAGS
-    .long CHECKSUM
+.section .multiboot2
+    .align 8
+multiboot2_header:
+    .long MULTIBOOT2_HEADER_MAGIC
+    .long MULTIBOOT2_ARCHITECTURE_I386
+    .long MULTIBOOT2_HEADER_LENGTH
+    .long MULTIBOOT2_HEADER_CHECKSUM
 
-    # aout kludge (unused)
-    .long 0,0,0,0,0
+    // Tag: information request (optional, ask for memory map)
+    .short 1                  // tag type: information request
+    .short 0                  // reserved
+    .long  8 + 4              // size of tag
+    .long  6                  // mmap
+    .align 8
 
-    # Video mode
-    .long   1       # Linear graphics please?
-    .long   1024       # Preferred width
-    .long   768      # Preferred height
-    .long   32      # Preferred pixel depth
+    // framebuffer
+    .short 5                 // type: framebuffer
+    .short 0
+    .long 20
+    .long 1024               // width
+    .long 768                // height
+    .long 32                 // bpp
+    .align 8, 0
 
+    // end
+    .short 0                 // tag type: end
+    .short 0                 // reserved
+    .long 8                  // size of tag
+    .align 8
+multiboot2_header_end:
+
+// Kernel entry
 .section .text
 .global _start
 .type _start, @function
@@ -28,16 +43,19 @@
 
 _start:
     mov $kernel_stack, %esp
-    
-    /* parameters to kernel_entry, not required, but we still accept it. */
-    push %eax
-    push %ebx
-    call kmain /* let's fucking go */
-loop:
+
+    // Multiboot2 passes boot info in %ebx
+    pushl %ebx         # Multiboot info pointer
+    pushl %eax         # Multiboot magic
+    call kmain
+
+.hang:
     cli
     hlt
-    jmp loop
+    jmp .hang
 
+// Reserve stack
 .section .bss
-.space 2*1024*1024 # 2 MiB
+    .align 16
+    .space 8*1024*1024   // 2 MiB stack
 kernel_stack:
