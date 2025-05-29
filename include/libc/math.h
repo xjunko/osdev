9c02f8098dfde58785704ac89e3e5b36193a0651
xjunko <fireredz69420@gmail.com>
2025-05-29 16:59:04 +0800

    libc: basic math.h

---
+++ include/libc/math.h
@@ -0,0 +1,5 @@
+#pragma once
+#include <kernel/types.h>
+
+int round(int);
+f32 roundf(f32);
---
+++ src/libc/math/round.c
@@ -0,0 +1,4 @@
+#include <kernel/types.h>
+#include <math.h>
+
+int round(int i) { return (int)roundf((f32)i); }
