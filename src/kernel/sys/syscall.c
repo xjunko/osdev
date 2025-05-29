ddcd5140cc53e4a91cb2eafa1ecb306dd5c4cf17
xjunko <fireredz69420@gmail.com>
2025-05-30 03:23:03 +0800

    syscall: stubs

--- src/kernel/sys/syscall.c
+++ src/kernel/sys/syscall.c
@@ -4,16 +4,19 @@
 #include <kernel/types.h>
 #include <stdio.h>
 
-// linux has 256, this should be enough for now
-#define SYSCALL_N 256
+// linux style, this should be enough for now
+#define SYSCALL_N 420
 static syscall_callback* syscall_handlers[SYSCALL_N];
+
+void syscall_install();
 
 void syscall_init(u8 interrupt_number) {
   idt_set_handler(IRQ_BASE + interrupt_number, syscall_handle);
+  syscall_install();
 }
 
 void syscall_register(u32 syscall_number, syscall_callback* handler) {
-  if (syscall_number < 256) {
+  if (syscall_number < SYSCALL_N) {
     syscall_handlers[syscall_number] = handler;
   } else {
     printf("[SYSCALL] Invalid syscall number: %d\n", syscall_number);
@@ -32,4 +35,37 @@ u32 syscall_handle(u32 esp) {
   }
 
   return esp;
+}
+
+// implementation
+#define SYSCALL_EXIT 0x1
+static u32 syscall_exit(struct regs* r) {
+  printf("[syscall] exit!");
+  return (u32)r;
+}
+
+#define SYSCALL_FORK 0x2
+static u32 syscall_fork(struct regs* r) {
+  printf("[syscall] fork!");
+  return (u32)r;
+}
+
+#define SYSCALL_READ 0x3
+static u32 syscall_read(struct regs* r) {
+  printf("[syscall] read!");
+  return (u32)r;
+}
+
+#define SYSCALL_WRITE 0x4
+static u32 syscall_write(struct regs* r) {
+  printf("[syscall] write!");
+  return (u32)r;
+}
+
+// install
+void syscall_install() {
+  syscall_register(SYSCALL_EXIT, syscall_exit);
+  syscall_register(SYSCALL_FORK, syscall_fork);
+  syscall_register(SYSCALL_READ, syscall_read);
+  syscall_register(SYSCALL_WRITE, syscall_write);
 }
