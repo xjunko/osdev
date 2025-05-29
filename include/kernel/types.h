202b8819ac04258010c5fbbd766d37a41af9580c
xjunko <fireredz69420@gmail.com>
2025-05-29 17:58:03 +0800

    kernel/types: use stdint.h for size_t*

--- include/kernel/types.h
+++ include/kernel/types.h
@@ -1,12 +1,12 @@
 #pragma once
 #include <stdbool.h>
 #include <stddef.h>
+#include <stdint.h>
 
 typedef char int8;
 typedef int8 i8;
 typedef unsigned char uint8;
 typedef uint8 u8;
-typedef uint8 uint8_t;
 
 typedef short int16;
 typedef int16 i16;
@@ -17,13 +17,11 @@ typedef int int32;
 typedef int32 i32;
 typedef unsigned int uint32;
 typedef uint32 u32;
-typedef uint32 uint32_t;
 
 typedef long long int64;
 typedef int64 i64;
 typedef unsigned long long uint64;
 typedef uint64 u64;
-typedef uint64 uint64_t;
 
 typedef float f32;
 typedef double f64;
