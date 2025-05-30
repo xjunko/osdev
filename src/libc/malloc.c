bcc0f8757b00a9dffe9514c10432e6a78c160c87
xjunko <fireredz69420@gmail.com>
2025-05-30 19:18:46 +0800

    kernel/libc: deprecate kernel malloc in favour of newlib's
    this  finally works hoooly

--- src/libc/malloc.c
+++
@@ -1,7 +0,0 @@
-#include <kernel/memory.h>
-#include <kernel/types.h>
-#include <stdio.h>
-
-void* malloc(u32 size) { return kmalloc(size); }
-void* realloc(void* ptr, u32 size) { return krealloc(ptr, size); }
-void free(void* ptr) { kfree(ptr); }
