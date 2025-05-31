650efbfa43197c7431aee1c7a0cf729576fffbd8
xjunko <fireredz69420@gmail.com>
2025-05-31 12:24:51 +0800

    elf: stubs

---
+++ include/kernel/elf.h
@@ -0,0 +1,70 @@
+#pragma once
+#include <kernel/types.h>
+
+#define EI_CLASS 4
+#define EI_DATA 5
+#define EI_VERSION 6
+
+#define ELFCLASS32 1
+#define ELFDATA2LSB 1
+
+#define EM_386 3
+#define EV_CURRENT 1
+#define ET_EXEC 2
+#define ET_DYN 3
+
+#define PT_LOAD 1
+#define PT_DYNAMIC 2
+
+#define SHT_REL 9
+#define SHT_RELA 4
+
+static const uint8_t ELF_MAGIC[] = {0x7f, 'E', 'L', 'F'};
+
+struct elf_header {
+  uint8_t ident[16];
+  uint16_t type;
+  uint16_t machine;
+  uint32_t version;
+  uint32_t entry;
+  uint32_t phoff;
+  uint32_t shoff;
+  uint32_t flags;
+  uint16_t ehsize;
+  uint16_t phentsize;
+  uint16_t phnum;
+  uint16_t shentsize;
+  uint16_t shnum;
+  uint16_t shstrndx;
+};
+
+struct elf_program_header {
+  uint32_t type;
+  uint32_t offset;
+  uint32_t vaddr;
+  uint32_t paddr;
+  uint32_t filesz;
+  uint32_t memsz;
+  uint32_t flags;
+  uint32_t align;
+};
+
+struct elf_section_header {
+  uint32_t name;
+  uint32_t type;
+  uint32_t flags;
+  uint32_t addr;
+  uint32_t offset;
+  uint32_t size;
+  uint32_t link;
+  uint32_t info;
+  uint32_t addralign;
+  uint32_t entsize;
+};
+
+struct elf_relocation {
+  uint32_t offset;
+  uint32_t info;
+};
+
+int elf_run(u8*, int, char**);
---
+++ src/kernel/utils/elf.c
@@ -0,0 +1,79 @@
+#include <kernel/elf.h>
+#include <kernel/types.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+uint8_t is_elf(u8* buf) {
+  struct elf_header* header = (struct elf_header*)buf;
+
+  for (int i = 0; i < 4; i++) {
+    if (header->ident[i] != ELF_MAGIC[i]) {
+      printf("[elf] invalid elf format!\n");
+      return 0;
+    }
+  }
+  return 1;
+}
+
+uint8_t elf_compatibility(u8* buf) {
+  struct elf_header* header = (struct elf_header*)buf;
+
+  if (header->ident[EI_CLASS] != ELFCLASS32 ||
+      header->ident[EI_DATA] != ELFDATA2LSB ||
+      header->ident[EI_VERSION] != EV_CURRENT ||
+      (header->type != ET_EXEC && header->type != ET_DYN &&
+       header->machine != EM_386)) {
+    printf("[elf] incompatible elf format!\n");
+    return 0;
+  }
+  return 1;
+}
+
+int elf_run(u8* buf, int argc, char** argv) {
+  if (!is_elf(buf) || !elf_compatibility(buf)) {
+    return -1;
+  }
+
+  size_t size = 0;
+  struct elf_header* header = (struct elf_header*)buf;
+  struct elf_program_header* ph =
+      (struct elf_program_header*)(buf + header->phoff);
+
+  for (int i = 0; i < header->phnum; i++) {
+    if (size < ph[i].filesz + ph[i].vaddr) {
+      size = ph[i].filesz + ph[i].vaddr;
+    }
+  }
+
+  u8* proc = malloc(size);
+  if (proc == NULL) {
+    printf("[elf] failed to allocate memory for process!\n");
+    return -1;
+  }
+
+  for (int i = 0; i < header->phnum; i++) {
+    if (ph[i].type == PT_LOAD || ph[i].type == PT_DYNAMIC) {
+      for (size_t y = 0; y < ph[i].filesz; y++) {
+        proc[y + ph[i].vaddr] = buf[y + ph[i].offset];
+      }
+    }
+  }
+
+  struct elf_section_header* sh =
+      (struct elf_section_header*)(buf + header->shoff);
+
+  for (int i = 0; i < header->shnum; i++) {
+    struct elf_section_header* shdr = sh + i;
+    if (shdr->type == SHT_REL || shdr->type == SHT_RELA) {
+      struct elf_relocation* rel = (struct elf_relocation*)(buf + shdr->offset);
+      for (int j = 0; j < shdr->size / sizeof(struct elf_relocation); j++) {
+        *(uint32_t*)(proc + rel[j].offset) += (uint32_t)proc;
+      }
+    }
+  }
+
+  int (*run)(int, char**) = (int (*)(int, char**))(proc + header->entry);
+  int ret = run(argc, argv);
+  free(proc);
+  return ret;
+}
